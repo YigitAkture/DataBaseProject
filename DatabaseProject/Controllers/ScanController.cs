c211b9bcdd0cbcb0656abb7de0097d05affe63f1
YigitAkture <115189586+YigitAkture@users.noreply.github.com>
2024-01-03 20:56:24 +0300

    Home page and scan page added

---
+++ DatabaseProject/Controllers/ScanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseProject.Controllers
+{
+    public class ScanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
